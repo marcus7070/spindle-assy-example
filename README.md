d8b3fac9254a8f47329115a0a17f6d189f2b9437
Marcus Boyd <mwb@geosol.com.au>
2020-10-07 09:34:15 +1030

    not looking good
    struggling to get the assembly relationships to line up to where I want.
    Also struggling to follow the source code and see the maths behind the
    relationships.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a model of the vacuum attachment for my Workbee CNC router. There is a m
 
 The vacuum attachment and brackets are 3D printed from this model.
 
-I'm publishing this on GitHub because I feel there isn't enough examples of complicated [CadQuery](https://github.com/cadquery/cadquery). models. I also learn the most from reading others' code, so I hope someone can get some ideas from reading this. 
+I'm publishing this on GitHub because I feel there isn't enough examples of complicated [CadQuery](https://github.com/cadquery/cadquery) models. I also learn the most from reading others' code, so I hope someone can get some ideas from reading this. 
 
 ![Old screenshot](https://github.com/marcus7070/spindle-assy-example/raw/master/screenshot2020-09-11-125732.png)
 
@@ -12,10 +12,10 @@ I'm publishing this on GitHub because I feel there isn't enough examples of comp
 
 If you're lucky enough to use NixOS (and can use flakes, currently in the unstable branch but soon to be merged into the main branch), don't bother installing anything, just use this to run cq-editor:
 ```sh
-nix run github:marcus7070/cq-flake/14d05cee591dccf5d64fa0e502e6e381a531c718
+nix run github:marcus7070/cq-flake/11ef47f5aa465e0b1cf9bbc15999a5ed0b713ba9
 ```
 and this to create an environment with a cadquery-aware python-language-server (which will hopefully be picked up by your IDE):
 ```sh
-nix shell github:marcus7070/cq-flake/14d05cee591dccf5d64fa0e502e6e381a531c718#cadquery-env
+nix shell github:marcus7070/cq-flake/11ef47f5aa465e0b1cf9bbc15999a5ed0b713ba9#cadquery-env
 ```
 These commands will use pinned versions of everything, from CadQuery to glibc, and should be completly reproducible no matter what happens to CadQuery, conda, pypi, NixOS, or even QT. Hooray for nix!
--- assembly.py
+++ assembly.py
@@ -6,35 +6,41 @@ import spindle as spindle_module
 import vac_brack as vac_brack_module
 import vac as vac_module
 import dims
-import importlib
-importlib.reload(dims)
-importlib.reload(clamp_module)
-importlib.reload(bracket_module)
-importlib.reload(spindle_module)
-importlib.reload(vac_brack_module)
-importlib.reload(vac_module)
+# import importlib
+# importlib.reload(dims)
+# importlib.reload(clamp_module)
+# importlib.reload(bracket_module)
+# importlib.reload(spindle_module)
+# importlib.reload(vac_brack_module)
+# importlib.reload(vac_module)
 
+assy = cq.Assembly()
 
-back = vslot.cslot(150)
-clamp = (
-    clamp_module.clamp
-    .clean()
-    .rotate((0, 0, 0), (0, 0, 1), 180)
-    .translate(dims.assembly.clamp.offset)
-)
-bracket = (
-    bracket_module.bracket
-    .translate(dims.assembly.bracket.offset)
-)
-spindle = (
-    spindle_module.spindle
-    .translate(dims.assembly.spindle.offset)
-)
-vac_brack = (
-    vac_brack_module.bracket
-    .translate(dims.assembly.vac_brack.offset)
-)
-vac = (
-    vac_module.part
-    .translate(dims.assembly.vac.offset)
-)
+assy.add(vslot.cslot(250), name="back", color=cq.Color(0.7, 0.7, 0.7))
+assy.add(bracket_module.bracket, name="bracket", color=cq.Color(0.8, 0.8, 0.85))
+assy.constrain("bracket@faces@>Y", "back@faces@<Y", "Plane")
+assy.solve()
+show_object(assy)
+
+# clamp = (
+#     clamp_module.clamp
+#     .clean()
+#     .rotate((0, 0, 0), (0, 0, 1), 180)
+#     .translate(dims.assembly.clamp.offset)
+# )
+# bracket = (
+#     bracket_module.bracket
+#     .translate(dims.assembly.bracket.offset)
+# )
+# spindle = (
+#     spindle_module.spindle
+#     .translate(dims.assembly.spindle.offset)
+# )
+# vac_brack = (
+#     vac_brack_module.bracket
+#     .translate(dims.assembly.vac_brack.offset)
+# )
+# vac = (
+#     vac_module.part
+#     .translate(dims.assembly.vac.offset)
+# )
